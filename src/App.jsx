265a2c38e2ffe9bb6a7d06d873c4795c7d7a70a3
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-15 15:41:28 -0300

    feat: create routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Artists from "./pages/Artists.jsx";
+import Artist from "./pages/Artist.jsx";
+import Songs from "./pages/Songs.jsx";
+import Song from "./pages/Song.jsx";
 
 const App = () => {
-  return (
-    <>
-      <Header/>
-      <Main/>
-    </>
-  )
-}
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/artists" element={<Artists />} />
+                <Route path="/artist/:id" element={<Artist />} />
+                <Route path="/songs" element={<Songs />} />
+                <Route path="/song/:id" element={<Song />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
