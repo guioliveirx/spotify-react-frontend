72584665bf54246878728e2e871afd7072f22671
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-16 16:28:29 -0300

    feat: implement Artist and Song pages with SongList and Player components

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import SongList from "../components/SongList";
 
 const Artist = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="artist">
+            <div
+                className="artist__header"
+                style={{
+                    backgroundImage:
+                        "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://i.scdn.co/image/ab67618…)",
+                }}
+            >
+              <h2 className="artist__title">Jorge e Matheus</h2>
+            </div>
 
-export default Artist
+            <div className="artist__body">
+              <h2>Populares</h2>
+              <SongList/>
+            </div>
+            <Link to="/song/1">
+                <FontAwesomeIcon
+                    className="single-item__icon single-item__icon--artist"
+                    icon={faCirclePlay}
+                />
+            </Link>
+        </div>
+    );
+};
+
+export default Artist;
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import Player from "../components/Player";
 
 const Song = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="song">
+            <div className="song__container">
+                <div className="song__image-container">
+                    <img
+                        src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+                        alt="Imagem de uma Música"
+                    />
+                </div>
+            </div>
+            <div className="song__bar">
+                <div className="song__artist-image">
+                    <img
+                        width={75}
+                        height={75}
+                        src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                        alt="Imagem de um artista"
+                    />
+                </div>
 
-export default Song
+                <Player />
+
+                <div>
+                    <p className="song__name">Última Saudade - Ao Vivo</p>
+                    <p>Henrique & Juliano</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Song;
