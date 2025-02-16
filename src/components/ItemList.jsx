b152ded713ce3883e3880b3012e5b9993ae62ca7
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-16 10:22:38 -0300

    feat: render ItemList based on pathname

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import SingleItem from "./SingleItem";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+    const { pathname } = useLocation();
+    const isHome = pathname === "/";
+    const finalItems = isHome ? items : Infinity
+
     return (
         <div>
             <div className="item-list">
                 <div className="item-list__header">
                     <h2>{title} Populares</h2>
-                    <Link className="item-list__link" to={path}>
-                        Mostrar tudo
-                    </Link>
+                    {isHome && (
+                        <Link className="item-list__link" to={path}>
+                            Mostrar tudo
+                        </Link>
+                    )}
                 </div>
                 <div className="item-list__container">
                     {itemsArray
-                        .filter((value, index) => index < items)
+                        .filter((value, index) => index < finalItems)
                         .map((object, index) => (
-                            <SingleItem idPath={idPath} {...object} key={`${title}-${index}`} />
+                            <SingleItem
+                                idPath={idPath}
+                                {...object}
+                                key={`${title}-${index}`}
+                            />
                         ))}
                 </div>
             </div>
