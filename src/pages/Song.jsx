f87d57ea146efa281f94b7b8e6f0990c3839671e
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-17 00:04:32 -0300

    feat: update Song component to display dynamic song and artist data

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import Player from "../components/Player";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { songsArray } from "../assets/database/songs";
+import { artistArray } from "../assets/database/artists";
 
 const Song = () => {
+    const { id } = useParams();
+
+    const { image, name, duration, audio, artist } = songsArray.filter(
+        (currentSongObj) => currentSongObj.id === Number(id)
+    )[0];
+
+    const artistObj = artistArray.filter(
+        (currentArtistObj) => currentArtistObj.name === artist
+    )[0];
+
     return (
         <div className="song">
             <div className="song__container">
                 <div className="song__image-container">
-                    <img
-                        src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
-                        alt="Imagem de uma Música"
-                    />
+                    <img src={image} alt={`Imagem da música ${name}`} />
                 </div>
             </div>
             <div className="song__bar">
-                <Link to="/artist/1" className="song__artist-image">
+                <Link to={`/artist/${artistObj.id}`} className="song__artist-image">
                     <img
                         width={75}
                         height={75}
-                        src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
-                        alt="Imagem de um artista"
+                        src={image}
+                        alt={`Imagem do(a) ${artist}`}
                     />
                 </Link>
 
                 <Player />
 
                 <div>
-                    <p className="song__name">Última Saudade - Ao Vivo</p>
-                    <p>Henrique & Juliano</p>
+                    <p className="song__name">{name}</p>
+                    <p>{artist}</p>
                 </div>
             </div>
         </div>
